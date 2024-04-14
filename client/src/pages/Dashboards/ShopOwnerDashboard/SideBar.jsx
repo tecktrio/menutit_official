7496ab8e713682a47200a407365eea2a40f48acf
Muhammed Aslam TS <taslam8797@gmail.com>
2024-04-15 02:24:58 +0530

    register

--- client/src/pages/Dashboards/ShopOwnerDashboard/SideBar.jsx
+++ client/src/pages/Dashboards/ShopOwnerDashboard/SideBar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import FontAwesomeIcon from "@fortawesome/react-fontawesome"
-import { faGauge } from "@fortawesome/fa-gauge"; // Import specific 
+// import FontAwesomeIcon from "@fortawesome/react-fontawesome"
+// import { faGauge } from "@fortawesome/fa-gauge"; // Import specific 
 
 
 export const SideBar = () => {
@@ -8,7 +8,7 @@ export const SideBar = () => {
     <div className="w-1/5 bg-red-400">
       <div id="sidebar" className="bg-blue-950 w-full h-screen">
         <div className="pl-7 pt-5">
-        <FontAwesomeIcon icon={faGauge} /> 
+        {/* <FontAwesomeIcon icon={faGauge} />  */}
 
           <p className="text-gray-50 pb-3">Dashboard</p>
           <p className="text-gray-50 pb-3">Account</p>
